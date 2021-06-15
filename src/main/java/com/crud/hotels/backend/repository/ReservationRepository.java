66c5f481fcbc10cc84ad2762c6eb9ae1e6428b5b
Danielo <nazguleczek@gmail.com>
2021-06-15 22:19:12 +0200

    Poprawki

--- src/main/java/com/crud/hotels/backend/repository/ReservationRepository.java
+++ src/main/java/com/crud/hotels/backend/repository/ReservationRepository.java
@@ -2,6 +2,7 @@ package com.crud.hotels.backend.repository;
 
 import com.crud.hotels.backend.domain.Reservation;
 import com.crud.hotels.backend.domain.User;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
@@ -9,7 +10,7 @@ import java.util.List;
 import java.util.Optional;
 
 @Repository
-public interface ReservationRepository extends CrudRepository<Reservation, Long> {
+public interface ReservationRepository extends JpaRepository<Reservation, Long> {
     List<Reservation> findAllByUser(User user);
 
     Optional<Reservation> getReservationById(Long id);
